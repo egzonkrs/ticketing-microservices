5bd55af6fbf9c9a21a0bb9725f392b3085a9e035
egzonkrs <71973901+egzonkrs@users.noreply.github.com>
2021-11-17 16:59:38 +0100

    added docker images, skaffold, kubernetes pods and ingress nginx controllers

---
+++ auth/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+
+const port: number = 3000 || process.env.PORT;
+const app = express();
+app.use(json());
+
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(currentUserRouter);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('[Auth] - Listening on portt: ' + port);
+});
