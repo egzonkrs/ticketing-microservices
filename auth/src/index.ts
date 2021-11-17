e3abaf3986df1aae7fb141741f34ede45c95f0e4
egzonkrs <71973901+egzonkrs@users.noreply.github.com>
2021-11-18 00:36:14 +0100

    password hashing using scrypt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const port: number = 3000 || process.env.PORT;
 const app = express();
@@ -16,8 +19,23 @@ app.use(signupRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
 
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log('[Auth] - Listening on portt: ' + port);
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to mongodb');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(port, () => {
+    console.log('[Auth] - Listening on port: ' + port);
+  });
+};
+
+start();
